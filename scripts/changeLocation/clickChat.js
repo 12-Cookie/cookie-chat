907b28faf227000ce4f7f7bd6e754fa43a82c9f0
Yonghun <yonghuni484@gmail.com>
2023-08-29 20:34:02 +0900

    Feat: get roomdata and view

--- scripts/changeLocation/clickChat.js
+++ scripts/changeLocation/clickChat.js
@@ -1,14 +1,18 @@
-const chatEls = document.querySelectorAll("#chat-views ul .chat-card");
+// const chatEls = document.querySelectorAll("#chat-views ul .chat-card");
 
-chatEls.forEach((chatEl) => {
+const clickChat = (chatEl, id) => {
   chatEl.addEventListener("click", () => {
     if (
       window.location.pathname === "/" ||
       window.location.pathname === "/index.html"
     ) {
-      window.location.href = "./views/chatRoom.html";
+      window.location.href = `./views/chatRoom.html?id=${id}`;
     } else {
-      window.location.href = "./chatRoom.html";
+      window.location.href = `./chatRoom.html?id=${id}`;
     }
   });
-});
+};
+
+export default clickChat;
+
+// chatEls.forEach((chatEl) => {});
