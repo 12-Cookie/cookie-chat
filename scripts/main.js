10c76076798fb5df953c843444431f218676d4fb
Yonghun <yonghuni484@gmail.com>
2023-08-30 21:12:45 +0900

    Feat: get myChat, likeChat data and view

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const user = {
+  id: "xzIZIHffe8QpmF8GnBN6",
+  email: "test@test.com",
+  isLogin: "true",
+  likes: ["sCBgQQYu52Au7HTdbq7g", "3v6iOUQhzX9WA1Qer4Ao"],
+  name: "user1",
+  phone: "010-1111-1111",
+};
+
+localStorage.setItem("user", JSON.stringify(user));
