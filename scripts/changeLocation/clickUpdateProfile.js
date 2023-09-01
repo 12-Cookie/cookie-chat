12270fcdb378fdb6cad0ea46cc655e04378d7452
Yonghun <yonghuni484@gmail.com>
2023-09-02 00:03:53 +0900

    Fix: update profile

--- scripts/changeLocation/clickUpdateProfile.js
+++ scripts/changeLocation/clickUpdateProfile.js
@@ -1,47 +1,5 @@
-// import db from "../firebase.js";
-// import { doc, setDoc, getDocs, collection } from "firebase/firestore";
-
 const goUpdatePage = document.querySelector(".go-update-page");
-const goProfilePage = document.querySelector(".go-profile-page");
-// const url = new URL(window.location);
-// const urlParams = url.searchParams;
-// const userID = urlParams.get('id');
 
-const userID = JSON.parse(localStorage.getItem("user")).id;
-
-// const userSnap = await getDocs(collection(db, "users"));
-// const userDoc = userSnap.docs.filter((doc) => doc.id === userID);
-// const userDisplayName = document.getElementById("userDisplayName");
-// userDisplayName.innerText = userDoc[0].data().name;
-
-if (goUpdatePage) {
-  goUpdatePage.addEventListener("click", () => {
-    window.location.href = `./updateMyPage.html?id=${userID}`;
-  });
-}
-
-if (goProfilePage) {
-  goProfilePage.addEventListener("click", () => {
-    window.location.href = "./myPage.html";
-  });
-}
-
-const setDocOptions = {
-  merge: true,
-};
-
-// const submitBtn = document.querySelector(".submit-btn");
-// submitBtn.addEventListener("click", async (e) => {
-//   e.preventDefault();
-//   await setDoc(
-//     doc(db, "users", userID),
-//     {
-//       name: nameInput.value,
-//       email: emailInput.value,
-//       phone: phoneInput.value,
-//     },
-//     setDocOptions
-//   ).then(() => {
-//     window.location.href = "/";
-//   });
-// });
+goUpdatePage.addEventListener("click", () => {
+  window.location.href = `./updateMyPage.html`;
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
 const user = {
-  id: "wqi1uWlfsZRmAmcO2AiCJ7EG8hQ2",
-  email: "yonghuni@cookie.com",
-  likes: ["sCBgQQYu52Au7HTdbq7g", "3v6iOUQhzX9WA1Qer4Ao"],
+  id: "kcCdVersJyOexCoxZUf9f5I0jqv1",
+  email: "yong@co.com",
+  likes: [],
   name: "이용훈",
-  phone: "010-4768-7401",
 };
 
 localStorage.setItem("user", JSON.stringify(user));
