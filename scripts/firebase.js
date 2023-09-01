f7971a7d5aae5412a7636f51d367d4396dd15a38
seungsimdang <oak20005@naver.com>
2023-09-01 18:31:15 +0900

    Feat: env파일 추가

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,18 +1,17 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js';
-// import { getAnalytics } from 'firebase/analytics';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBETTxIGPFcBZQG0UIvx-rAWEp3EPJEEpY',
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: 'project-728bb.firebaseapp.com',
   projectId: 'project-728bb',
   storageBucket: 'project-728bb.appspot.com',
   messagingSenderId: '946069755767',
-  appId: '1:946069755767:web:6dd30afaf29be7e10eb372',
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
   measurementId: 'G-76HF9KWZHY',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
