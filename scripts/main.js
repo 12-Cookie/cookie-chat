f9312c449028576d1bdf084ff1c12f8bfd533557
Yonghun <yonghuni484@gmail.com>
2023-09-04 00:10:43 +0900

    Remove main.js

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-// const user = {
-//   id: "kcCdVersJyOexCoxZUf9f5I0jqv1",
-//   email: "yong@co.com",
-//   likes: [],
-//   name: "이용훈",
-// };
-
-// localStorage.setItem("user", JSON.stringify(user));
