0290e32e45d70bc8a1525ce0ddda659d4e9c41bc
Taegwan Youn <tkyoun0421@naver.com>
2023-09-06 10:06:53 +0900

    Fix: signin.css 추가

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,18 +1,18 @@
-const mainContent = document.querySelector("main");
-const loadingEl = document.querySelector(".loading");
+const mainContent = document.querySelector('main');
+const loadingEl = document.querySelector('.loading');
 
 const loading = (loading) => {
-  if (loading) {
-    mainContent.style.display = "none";
-    if (loadingEl) {
-      loadingEl.style.display = "inline-block";
+    if (loading) {
+        mainContent.style.display = 'none';
+        if (loadingEl) {
+            loadingEl.style.display = 'inline-block';
+        }
+    } else {
+        mainContent.style.display = 'block';
+        if (loadingEl) {
+            loadingEl.style.display = 'none';
+        }
     }
-  } else {
-    mainContent.style.display = "block";
-    if (loadingEl) {
-      loadingEl.style.display = "none";
-    }
-  }
 };
 
 export default loading;
